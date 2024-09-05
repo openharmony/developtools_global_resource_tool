24078f1088c1a49c7d4c8086a81f7415e74fbee7
@ran-zhao-yu <ranzhaoyu1@huawei.com>
2024-09-05 16:55:20 +0800

    蓝黄同步
    Signed-off-by: @ran-zhao-yu <ranzhaoyu1@huawei.com>

--- include/resource_data.h
+++ include/resource_data.h
@@ -48,7 +48,7 @@ const static std::string SOLUTIONS_ARROW = "> ";
 const static std::string LONG_PATH_HEAD = "\\\\?\\";
 const static int32_t VERSION_MAX_LEN = 128;
 const static int32_t INT_TO_BYTES = sizeof(uint32_t);
-static const int8_t RESTOOL_VERSION[VERSION_MAX_LEN] = { "Restool 5.010" };
+static const int8_t RESTOOL_VERSION[VERSION_MAX_LEN] = { "Restool 5.009" };
 const static int32_t TAG_LEN = 4;
 static std::set<std::string> g_resourceSet;
 
